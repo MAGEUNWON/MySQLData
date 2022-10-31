976f69f1e5ad765ed7bc4977ada7fbc777963835
mageunwon <geunwon1947@gmail.com>
2022-10-31 22:18:39 +0900

    poster 테이블에 넣기

---
+++ config/mysql.js
@@ -0,0 +1,45 @@
+const mysql = require('mysql');
+
+
+// const dbconfig = {
+//   host: 'localhost',
+//   port: '3306',
+//   user: 'root',
+//   password: 'root',
+//   database: 'inthem'
+// }
+
+// const conenction = mysql.createConnection(dbconfig);
+
+const mysqlConnection = {
+  init : function(){
+    return mysql.createConnection({
+      host: process.env.host,
+      port: process.env.port,
+      user: process.env.user,
+      password: process.env.password,
+      database: process.env.database,
+      multipleStatements : true  //여러 쿼리를 ';' 기준으로 한번에 보낼 수 있게 함.
+    });
+  },
+  open: (con)=>{
+    con.connect(err =>{
+      if(err){
+        console.log("MySQL 연결 실패 : ", err);
+      } else{
+        console.log("MySQL Connected!");
+      }
+    });
+  },
+  close: (con)=>{
+    con.end(err =>{
+      if(err) {
+        console.log("MySQL 종료 실패 :", err);
+      } else{
+        console.log("MySQL Terninated");
+      }
+    })
+  }
+}
+
+module.exports = mysqlConnection;
